4d024aaa7aac345307892ad74ee76cd64887ddf6
eyngqin <yang.qin@ericsson.com>
2022-02-14 15:52:18 +0800

    qinyang 3

--- README.md
+++ README.md
@@ -8,11 +8,19 @@ Maven
 Spring Boot 
 RestTemplate
 
-访问方法如下：
-http://localhost:8087/cityName/{cityName}
-http://localhost:8087/cityId/{cityID}
+部署方式使用了阿里云的流水线，做到了CI/CD，做到了云部署，
+拉取我在github上的代码，执行pom.xml，打包，并部署
+代码在github上,使用deploy.sh的shell脚本部署
+github: https://github.com/eyngqin/weather-app
+
+访问方法如下：下面是多个城市天气预报的访问
+http://47.104.90.206:8087/cityNames/{cityNames}
+这里括号中的城市可以填写多个，以逗号分隔例如
+http://47.104.90.206:8087/cityNames/青岛,大连,北京
+
+下面是单个城市的天气预报访问，可以输入城市ID
+http://47.104.90.206:8087/cityId/{cityID}
 for instance:
-http://localhost:8087/cityName/北京
-http://localhost:8087/cityId/101280601
+http://47.104.90.206:8087/cityId/101280601
 
 
--- src/test/java/com/example/weather/WeatherApplicationTests.java
+++ src/test/java/com/example/weather/WeatherApplicationTests.java
@@ -1,14 +1,18 @@
 package com.example.weather;
 
+import com.example.weather.entity.Forecast;
 import com.example.weather.entity.WeatherResponse;
 import com.example.weather.service.WeatherDataService;
 
 
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -38,5 +42,16 @@ public class WeatherApplicationTests {
 
         assertEquals(cityName, weatherResponse.getData().getCity());
     }
+
+
+    @Test
+    public void getDataByCityNames() {
+
+        String cityName = "北京,上海";
+        List<Forecast> list = weatherDataService.getDataByCityNames(cityName);
+
+        assertEquals(cityName.split(",")[0], list.get(0).getCity());
+    }
+
 
 }
