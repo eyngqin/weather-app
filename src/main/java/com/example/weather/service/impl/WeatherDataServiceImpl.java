6d771604acd399eb534e8739e000e02670e3fe5e
eyngqin <yang.qin@ericsson.com>
2022-02-14 16:05:59 +0800

    optimized code

--- src/main/java/com/example/weather/service/impl/WeatherDataServiceImpl.java
+++ src/main/java/com/example/weather/service/impl/WeatherDataServiceImpl.java
@@ -8,6 +8,7 @@ import org.apache.tomcat.util.http.parser.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -23,18 +24,21 @@ import java.util.concurrent.TimeUnit;
 public class WeatherDataServiceImpl implements WeatherDataService {
     @Autowired
     private RestTemplate restTemplate;
-    private final String WEATHER_API = "http://wthrcdn.etouch.cn/weather_mini";
+    @Value("${weather.api.cityid}")
+    private String WEATHER_API_CITYID;
+    @Value("${weather.api.cityname}")
+    private String WEATHER_API_CITYNAME;
 
 
     @Override
     public WeatherResponse getDataByCityId(String cityId) {
-        String uri = WEATHER_API + "?citykey=" + cityId;
+        String uri = WEATHER_API_CITYID + cityId;
         return this.doGetWeatherData(uri);
     }
 
     @Override
     public WeatherResponse getDataByCityName(String cityName) {
-        String uri = WEATHER_API + "?city=" + cityName;
+        String uri = WEATHER_API_CITYNAME + cityName;
         return this.doGetWeatherData(uri);
     }
 
@@ -43,7 +47,7 @@ public class WeatherDataServiceImpl implements WeatherDataService {
         String[] citys = cityNames.split(",");
         List<Forecast> list = new ArrayList<Forecast>();
         for (String cityName: citys) {
-            Forecast f = this.doGetWeatherData(WEATHER_API + "?city=" + cityName)
+            Forecast f = this.doGetWeatherData(WEATHER_API_CITYNAME + cityName)
                     .getData().getForecast().get(0);
             f.setCity(cityName);
             list.add(f);
